899987a4ff274ff94dc2f65a60567582c6e0a9b3
astha9120 <62197051+astha9120@users.noreply.github.com>
2021-04-02 21:13:36 +0530

    Add files via upload

--- project work/front_backend/controllers/clientController.js
+++ project work/front_backend/controllers/clientController.js
@@ -172,3 +172,92 @@ exports.withdrawMoneyPost=async(req,res)=>{
         res.status(404).send();
     }
 }
+
+exports.getBalance = async(req,res)=>{
+    try {
+        const balance = await Client.find({id:1},{Primary_balance:1,_id:0,Saving_balance:1});
+        console.log(balance[0]);
+        res.status(200);
+        res.render('balance',{Primary_balance:balance[0].Primary_balance,Saving_balance:balance[0].Saving_balance})
+    } catch (error) {
+        res.status(404).json({
+            status:'fail',
+            message:error
+        })
+    }
+}
+
+exports.getProfile=async(req,res)=>{
+    try {
+        const profile = await Client.find({id:1},{id:0,_id:0});
+        console.log(profile);
+        res.status(200);
+        res.render('profile_final',{profile:profile[0]});
+        
+    } catch (error) {
+        res.status(404).json({
+            status:'fail',
+            message:error
+        })
+    }
+}
+
+
+exports.updateProfile = async(req,res)=>{
+    
+   const Email_id = String(req.body.email);
+    const Address=String(req.body.address);
+    const Phone_number =String(req.body.phone_num);
+    console.log("emails::::"+Email_id);
+    try {
+       res.status(200);
+       Client.findOne({"id":1})
+       .then((result)=>{
+        if(req.body.submit == "submit"){
+            result.Email_id=Email_id;
+            result.Address=Address;
+            result.Phone_number=Phone_number;
+            result.save();
+            console.log(result);
+            res.redirect("/clients/profile");
+            }
+       }) 
+       .catch((err)=>
+         {
+           res.status(404).send();
+           console.log(err);
+         })
+        
+    } catch (err) {
+        res.status(404).json({
+            status:'fail',
+            message:error
+        })
+    }
+}
+
+
+
+exports.chequebook= async(req,res)=>{
+    try {
+        res.status(200);
+        res.render('checkbook_home');
+    } catch (error) {
+        res.status(404).send();
+    }
+}
+
+exports.chequebook_confirm= async(req,res)=>{
+    try {
+        if(req.body.submit=="Yes"){
+            const address = await Client.find({id:1},{Address:1,_id:0});
+            console.log(address[0]);
+            res.render("checkbook_confirm",{address:address[0].Address});
+        }
+        else
+            res.redirect("/clients/balance");
+
+    } catch (error) {
+        res.status(404).send();
+    }
+}
